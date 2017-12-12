27872ed4abba078eb04de35a67f015e1cb956949
Bhebhe <linda.bhebhe@ashesi.edu.gh>
2017-12-12 20:07:37 +0100

    after editing all files

--- platforms/browser/www/js/myscript.js
+++ platforms/browser/www/js/myscript.js
@@ -1,98 +1,93 @@
 
-
+/*Funtion to validate the registration page 
+  retrieves the register required details 
+  and prompts user if any os them is
+*/
 function validateregistration(){	
-	//These are the commands for getting the elements from the html file
-	var fname = document.getElementById("fnm");
-	var lname = document.getElementById("lnm");
-	var uname = document.getElementById("unm");
+	var username = document.getElementById("unm");
 	var email = document.getElementById("eml");
 	var age = document.getElementById("age");
-	var pnumber = document.getElementById("pnm");
-	var uerror = document.getElementById("uerror");
-	var perror = document.getElementById("perror");
-	var emerror = document.getElementById("emerror");
-	var ferror = document.getElementById("ferror");
-	var lerror = document.getElementById("lerror");
-	var gerror = document.getElementById("gerror");
+	var phonenumber = document.getElementById("pnm");
+	var usererror = document.getElementById("uerror");
+	var phoneerror = document.getElementById("perror");
+	var emailerror = document.getElementById("emerror");
+	var agerror = document.getElementById("gerror");
 	var merror = document.getElementById("merror");
 
 	//validating username
-	if(uname.value==""){
-		uerror.innerHTML = "Please enter your username";
+	if(username.value==""){
+		usererror.innerHTML = "Username required";
 	}else{
-		if(!preg_match('/^[a-zA-Z ]+$/', uname)){
-			uerror.innerHTML = "Enter only alphabetical letters";
-		}		
-	}
-
-	//validating firstname
-	if(fname.value==""){
-		ferror.innerHTML = "Please enter your first name";
-	}else{
-		if(!preg_match('/^[a-zA-Z ]+$/', fname)){
-			ferror.innerHTML = "Enter only alphabetical letters";
-		}		
-	}
-
-	//validating lastname
-	 if(lname.value==""){
-		lerror.innerHTML = "Please enter your last name";
-	}else{
-		if(!preg_match('/^[a-zA-Z ]+$/', lname)){
-			lerror.innerHTML = "Enter only alphabetical letters";
+		if(!preg_match('/^[a-zA-Z ]+$/', username)){
+			usererror.innerHTML = "Only alphabetical letters allowed";
 		}		
 	}
 
 	//validating email
 	if(email.value==""){
-		emerror.innerHTML = "Enter your email address";
+		emailerror.innerHTML = "Email address required";
 	}else{
 	  if(!preg_match('/^\w+([\.-]?\w+)*@\w+([\.-]??\w+)*(\.\w{2,3})+$/', email)){
-			emerror.innerHTML = "Enter your email in the correct format";
+			emailerror.innerHTML = "Wrong email format";
 	  }		
 	}
 
 	//validating age
-	 if(lname.value==""){
-		lerror.innerHTML = "Please enter your age";	
+	 if(age.value==""){
+		agerror.innerHTML = "Age required";	
 	}
 	
 	//validating phonenumber
-	 if(lname.value==""){
-		lerror.innerHTML = "Please enter your phonenumber";
+	 if(phonenumber.value==""){
+		phoneerror.innerHTML = "Phonenumber required";
 	}
 		
 }
 
+/*function to register as a user in the system
+    gets all the data from the register page and sends to the 
+    register.php which stores the data in the server.
+    It redirects the user to the login page 
+*/
+function registerAsUser(){
 
-//Function to send the message
-function sendMessage(){
-	var username = document.getElementById("unme");
-	var pnumber = document.getElementById("pnm");
-	var message= username+ "successfully registered to WatchMe";
-    var url = 'api.deywuro.com/bulksms/?username=AshesiMoney&password=ashesi@123&type=0&dlr=1&destination='+pnumber+'&source=GHeSecurity&message='+ message;
- 
-}
+	//get the values
+	var usern= document.getElementById('unme').value;
+	var pass= document.getElementById('passwd').value;
+	var age= document.getElementById('age').value;
+	var pnumber= document.getElementById('pnm').value;
+	var mail= document.getElementById('eml').value;
+
+	//open httpRequest
+	if (window.XMLHttpRequest) {
+				xhttp = new XMLHttpRequest();
+				} else {			
+				xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+			}xhttp.onreadystatechange = function() {
+				if (this.readyState == 4 && this.status == 200) {					
+					if (this.responseText == "successful") {
+                            console.log(this.responseText);
+                            window.location.href = "login/index.html";
+                        }else{
+                        	console.log("error occured");
+
+                        }						
+				}
+		  };
+
+		  //send data to the server
+		  xhttp.open("GET", "https://watchmeorg.000webhostapp.com/register.php?username="+usern+"&password="+pass+"&age="+age+"&phonenumber="+pnumber+"&email="+mail, true);		  
+		  xhttp.send();
+		}
 
 
+/*function to login as a user
+    function to login in the system
+    gets all the data from the login page and sends to the 
+    login.php which accesses the server and checks if the login credentials are valid.
+    If valid, the user is redirected to the upload page.
 
-//function to validate the login
-function validatelogin(){
-	var uname = document.getElementById("unme");
-	var password = document.getElementById("passwd");
-	var usererror = document.getElementById("usererror");
-	var passerror = document.getElementById("passerror");
-	
-	if(uname.value==""){
-		usererror.innerHTML = "Enter your username";
-	}	
-	if(password.value==""){
-		passerror.innerHTML = "Enter your password";
-	}
-}
-
-
-//function to login as a user
+*/
 function loginAsUser(){
 	var usern= document.getElementById('unme').value;
 	var pass= document.getElementById('passwd').value;
@@ -123,36 +118,43 @@ function loginAsUser(){
 		  xhttp.send();
 		}
 
+/*function to validate the login page
+   prompts user to enter details if not completed
+*/
+function validatelogin(){
+	var uname = document.getElementById("unme");
+	var password = document.getElementById("passwd");
+	var usererror = document.getElementById("usererror");
+	var passerror = document.getElementById("passerror");
+	
+	if(uname.value==""){
+		usererror.innerHTML = "Enter your username";
+	}	
+	if(password.value==""){
+		passerror.innerHTML = "Enter your password";
+	}
+}		
 
 
-//function to register as a user
-function registerAsUser(){
-	var usern= document.getElementById('unme').value;
-	var pass= document.getElementById('passwd').value;
-	var age= document.getElementById('age').value;
-	var pnumber= document.getElementById('pnm').value;
-	var mail= document.getElementById('eml').value;
-	if (window.XMLHttpRequest) {
-				xhttp = new XMLHttpRequest();
-				} else {			
-				xhttp = new ActiveXObject("Microsoft.XMLHTTP");
-			}xhttp.onreadystatechange = function() {
-				if (this.readyState == 4 && this.status == 200) {					
-					if (this.responseText == "successful") {
-                            console.log(this.responseText);
-                            window.location.href = "login/index.html";
-                        }else{
-                        	console.log("error occured");
 
-                        }						
-				}
-		  };
-		  xhttp.open("GET", "https://watchmeorg.000webhostapp.com/register.php?username="+usern+"&password="+pass+"&age="+age+"&phonenumber="+pnumber+"&email="+mail, true);		  
-		  xhttp.send();
-		}
-
-
-function upload(){
-	alert("successfully uploaded");
+/*Function to send the message to the user registered
+*/
+function sendMessage(){
+	var username = document.getElementById("unme");
+	var phonenumber = document.getElementById("pnm");
+	var message= username+ "successfully registered to WatchMe";
+    var url = 'api.deywuro.com/bulksms/?username=AshesiMoney&password=ashesi@123&type=0&dlr=1&destination='+phonenumber+'&source=WatchMe&message='+ message;
+ 
 }
+
+
+
+
+
+
+
+
+
+
+
 
--- www/js/plugins.js
+++ www/js/plugins.js
@@ -1,52 +1,71 @@
+
+//variables
 var camera;
 var gps;
 
-/* To get the corrent location of the whistle blower*/
-function getlocation(){
-navigator.geolocation.getCurrentPosition(onSuccess, onError);
+/*Function to take a picture using the camera plugin
+*/
+function takePicture(){
+	navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+    destinationType: Camera.DestinationType.DATA_URL });
+}
+//on success
+function onSuccess(imageURI) {
+    camera = imageURI;
+}
+//on error
+function onFail(message) {
+    alert('Failed because: ' + message);
 }
 
 
+
+
+/* Function to get the location of the whistle blower
+   By the time they send the report to the authorities
+*/
+function getlocation(){
+navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}
+//on success
 function onSuccess(position) {
     gps=position.coords.latitude+","+position.coords.longitude;    
      }
-
-
+//on error
 function onError(error) {
     alert('code: '    + error.code    + '\n' +
         'message: ' + error.message + '\n');
 }
 
-function takePicture(){
-	navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
-    destinationType: Camera.DestinationType.DATA_URL });
-}
 
-function onSuccess(imageURI) {
-    camera = imageURI;
-}
 
-function onFail(message) {
-    alert('Failed because: ' + message);
-}
 
-function upload(){
+
+/*Function to upload from gallery
+  posts to the uploadReport.php which communicates with the server
+  to store the data
+*/
+function sendReport(){
+
 	var category= document.getElementById('option').value;
 	var description= document.getElementById('description').value;
 	var image = camera;
+
+	//get the location before uploading 
 	getlocation();
-	var location=gps;
+
+    //get an HttpRequest
 	if (window.XMLHttpRequest) {
 				xhttp = new XMLHttpRequest();
 				} else {
-				// code for IE6, IE5
+			
 				xhttp = new ActiveXObject("Microsoft.XMLHTTP");
 			}
 			xhttp.onreadystatechange = function() {
                  console.log("in the on readyState");
 				if (this.readyState == 4 && this.status == 200) {					
 					if (this.responseText == "successful") {
-						//Parse the Json results from the server and format it to give the name.
+					
 						    console.log(this.responseText);
                             alert("Upload successful");
                             } else {
@@ -54,8 +73,9 @@ function upload(){
                             }
                           }
                         };
-		  //Direct it to the page that you want the results to be displayed on
-		  xhttp.open("GET", "https://unlawful.000webhostapp.com/uploadReport.php?category="+category+"&description="+description+"&image="+image+"&location="+location, true);		  
+		
+		  //post the data 
+		  xhttp.open("GET", "https://watchmeorg.000webhostapp.com/upload.php?category="+category+"&description="+description+"&image="+image+"&location="+gps, true);		  
 		  xhttp.send();
 		}
 
